c4c223de9f7dfd027d6faa6af954101e59d82a2c
Abdeljalil El Yasni <elyasn0a@bobst.com>
2022-01-25 18:49:13 +0100

    add identity

--- BlazorWebApp/Program.cs
+++ BlazorWebApp/Program.cs
@@ -1,4 +1,5 @@
 using BlazorLib;
+using BlazorLib.Data;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<Main>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
